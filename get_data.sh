12bbf9eaf18003994a78bdabd5485a4d41e5b892
Tim Brooks <brooks@cern.ch>
2017-01-03 21:16:21 +0000

    Fetch initial data
    Pull data linked from:
    https://www.reddit.com/r/Physics/comments/5lm0l9/skiing_gforce_animation/
    at:
    https://www.dropbox.com/sh/gp9vo89jws3vfyp/AABMoTD4Qf_wbMq2FnecHYz2a

---
+++ get_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Video file
+wget https://www.dropbox.com/sh/gp9vo89jws3vfyp/AAAPm5Vhm6YOIOVnVLd0cjFEa/GOPR0068.MP4
+
+# Data file
+wget https://www.dropbox.com/sh/gp9vo89jws3vfyp/AAD0OnhpHnrAwqZeKvSlYsH_a/Acceleration%20data.xlsx
