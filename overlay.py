175c5432fc4da6bf199a7f692029bef606515c3c
Tim Brooks <brooks@cern.ch>
2017-01-05 01:16:36 +0000

    Render data into a graph

--- overlay.py
+++ overlay.py
@@ -2,9 +2,12 @@
 """Overlay data onto video file"""
 
 from __future__ import print_function
+import matplotlib.pyplot as plt
+import numpy as np
 from numpy import empty as empty_array
-from moviepy.editor import (VideoFileClip, TextClip, CompositeVideoClip,
-                            concatenate)
+from moviepy.editor import (VideoFileClip, VideoClip, TextClip, ImageClip,
+                            CompositeVideoClip, concatenate)
+from moviepy.video.io.bindings import mplfig_to_npimage
 
 
 def make_overlays(data, form=lambda x: "Frame #%05d" % int(x)):
@@ -22,12 +25,71 @@ def make_overlays(data, form=lambda x: "Frame #%05d" % int(x)):
     return concatenate(overlays, method='compose')
 
 
-def render(clips, file_name="test.mp4"):
+def mplfig_to_mask(fig):
+    """Extract alpha channel of fig"""
+    fig.canvas.draw()
+    width, height = fig.canvas.get_width_height()
+    # Get alpha and RGB data
+    buf = fig.canvas.tostring_argb()
+    image = +np.fromstring(buf, dtype=np.uint8)
+    # Rescale image to float in range [0, 1]
+    return image.reshape(height, width, 4)[:, :, 0].astype(float) * (1/255)
+
+
+def make_graph_overlays(data, fill=False, ylim=None):
+    """Make overlay plot"""
+    window_size = 5
+    if ylim is None:
+        ylim = (0, np.max(data))
+    time = data[:, 0]
+    index = np.sum(data[:, 0] < window_size)
+    figure, axis = plt.subplots(1, figsize=(4, 2.5), facecolor=(1, 1, 1))
+    series, = axis.plot(data[:index, 0], data[:index, 1],
+                        lw=2, c='k')
+    axis.set_xlim(-window_size, 0)
+    axis.set_ylim(0, 3)
+    axis.set_ylabel("Acceleration [g]")
+    axis.legend(loc=2)
+    figure.tight_layout()
+
+    figure.patch.set_facecolor('none')
+    mask = ImageClip(mplfig_to_mask(figure), ismask=True)
+    figure.patch.set_facecolor('white')
+
+    if fill:
+        reference = np.ones(time.size) * fill
+
+    duration = data[-1, 0]
+    series.set_ydata(data[:, 1])
+
+    def plot(timestamp):
+        """make_frame function for VideoClip"""
+        offset_time = data[:, 0] - timestamp
+        series.set_xdata(offset_time)
+
+        # Remove the fill_between patches. See:
+        # http://stackoverflow.com/questions/16120801/matplotlib-animate-fill-between-shape
+        if fill:
+            for coll in axis.collections:
+                axis.collections.remove(coll)
+            axis.fill_between(offset_time, data[:, 1], reference,
+                              interpolate=True,
+                              where=data[:, 1] >= reference,
+                              facecolor='red')
+            axis.fill_between(offset_time, data[:, 1], reference,
+                              interpolate=True,
+                              where=data[:, 1] <= reference,
+                              facecolor='blue')
+        return mplfig_to_npimage(figure)
+
+    return VideoClip(plot, duration=duration).set_mask(mask)
+
+
+def render(clips, file_name="test.mp4", bitrate=30000):
     """Composite a stack of clips together and render to file"""
     composite = CompositeVideoClip(clips)
-    # TODO: Note the bitrate is fixed here:
-    composite.write_videofile(file_name, codec='mpeg4', bitrate="30000000",
-                              write_logfile=True)
+    composite.write_videofile(file_name, codec='mpeg4',
+                              bitrate=str(bitrate * 1000), write_logfile=True)
 
 
 def demo():
@@ -43,15 +105,13 @@ def demo():
     print("video frame length: %d s" % frames)
 
     numbers = empty_array([frames, 2], dtype=float)
-    index = 0
-    for number in numbers:
-        number[0] = frame_time
+    for index, number in enumerate(numbers):
+        number[0] = frame_time * index
         number[1] = index
-        index += 2
-        print(number)
 
     # Overlay frame number onto video
-    overlays = make_overlays(numbers, lambda x: "Frame #%05d" % int(x))
+    # overlays = make_overlays(numbers, lambda x: "Frame #%05d" % int(x))
+    overlays = make_graph_overlays(numbers)
     render([video, overlays])
 
 if __name__ == '__main__':
--- skig.py
+++ skig.py
@@ -1,21 +1,56 @@
 #!/usr/bin/env python3
 """Overlay G-load data on video"""
+# pylint: disable=star-args
 
 from __future__ import print_function
-from data import from_file, differentiate
-from overlay import make_overlays, render
+from os import path
+from argparse import ArgumentParser
 from moviepy.editor import VideoFileClip
+from data import from_file
+from overlay import make_graph_overlays, render
 
 
-def demo():
-    """Proof of principle"""
-    g_data = differentiate(from_file('Acceleration_data.xlsx',
-                                     'Corviglia 30.12.'))
-    # TODO: Ask for the time offset of data to video
-    g_data[0, 0] += 0.68
-    overlays = make_overlays(g_data, lambda x: "%4.2f g" % x)
-    video = VideoFileClip("GOPR0068.MP4", audio=True)
-    render([video, overlays], "output2.mp4")
+def get_args():
+    """Parse command line arguments"""
+    parser = ArgumentParser()
+    add = parser.add_argument
+    add('-i', '--input_video', action='store', default="GOPR0068.MP4")
+    add('-d', '--input_data', action='store', default='Acceleration_data.xlsx')
+    add('-o', '--output', action='store', default=None)
+    add('-s', '--offset', action='store', type=int, default=0.68,
+        help="Offset in seconds from data to video")
+    add('-p', '--preview', action='store', nargs=2, type=int, default=None,
+        help="Render preview from START to END", metavar=('START', 'END'))
+    add('-b', '--bitrate', action='store', type=int, default=30000,
+        help="Output bitrate in kbps")
+    add('-f', '--fill', action='store', type=float, default=False,
+        help="Fill plot to REF", metavar='REF')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    """Main interface"""
+    args = get_args()
+    g_data = from_file(args.input_data,
+                       'Corviglia 30.12.')
+    g_data[:, 0] += args.offset - g_data[0, 0]
+    overlays = make_graph_overlays(g_data, fill=args.fill)
+    video = VideoFileClip(args.input_video, audio=True)
+    if args.preview is None:
+        if args.output is None:
+            overlays = overlays.subclip(0, video.duration)
+            directory, name = path.split(args.input_video)
+            args.output = path.join(directory, "ski-G_" + name)
+    else:
+        if args.preview[0] < 0:
+            args.preview = [args.preview[0] + video.duration,
+                            args.preview[1] + video.duration]
+        overlays = overlays.subclip(*args.preview)
+        video = video.subclip(*args.preview)
+        if args.output is None:
+            args.output = "ski-G_preview.mp4"
+    render([video, overlays], args.output, args.bitrate)
 
 if __name__ == '__main__':
-    demo()
+    main()
